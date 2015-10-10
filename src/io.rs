62421dac8d4cdae33e7fd4ffaee6a28ae9f40bdf
Sam Rijs <srijs@airpost.net>
2015-10-11 02:12:49 +1100

    add mpsc sinks/sources

--- src/io.rs
+++ src/io.rs
@@ -1,8 +1,10 @@
-use std::io::{Result, Read, Write};
+use std::io;
+use std::io::{Read, Write};
+use std::sync::mpsc::{Receiver, RecvError, Sender, SyncSender, SendError};
 
-use super::{ConduitM, produce, consume, defer, Flush};
+use super::{ConduitM, produce, consume, defer, leftover, Flush};
 
-fn read<R: Read>(r: &mut R, z: usize) -> Result<Vec<u8>> {
+fn read<R: Read>(r: &mut R, z: usize) -> io::Result<Vec<u8>> {
     let mut v = Vec::with_capacity(z);
     unsafe { v.set_len(z); }
     match r.read(&mut v) {
@@ -15,7 +17,7 @@ fn read<R: Read>(r: &mut R, z: usize) -> Result<Vec<u8>> {
 }
 
 /// A conduit that produces bytes it reads from the given `Read`.
-pub fn reader<'a, R: 'a + Read>(mut r: R, z: usize) -> ConduitM<'a, (), Vec<u8>, Result<()>> {
+pub fn reader<'a, R: 'a + Read>(mut r: R, z: usize) -> ConduitM<'a, (), Vec<u8>, io::Result<()>> {
     defer().and_then(move |_| {
         match read(&mut r, z) {
             Err(e) => Err(e).into(),
@@ -31,7 +33,7 @@ pub fn reader<'a, R: 'a + Read>(mut r: R, z: usize) -> ConduitM<'a, (), Vec<u8>,
 }
 
 /// A conduit that consumes bytes and writes them to the given `Write`.
-pub fn writer<'a, W: 'a + Write>(mut w: W) -> ConduitM<'a, Vec<u8>, (), Result<()>> {
+pub fn writer<'a, W: 'a + Write>(mut w: W) -> ConduitM<'a, Vec<u8>, (), io::Result<()>> {
     consume().and_then(|vo: Option<Vec<u8>>| {
         match vo {
             None => Ok(()).into(),
@@ -43,7 +45,7 @@ pub fn writer<'a, W: 'a + Write>(mut w: W) -> ConduitM<'a, Vec<u8>, (), Result<(
     })
 }
 
-fn write_flush<W: Write>(w: &mut W, f: Flush<Vec<u8>>) -> Result<()> {
+fn write_flush<W: Write>(w: &mut W, f: Flush<Vec<u8>>) -> io::Result<()> {
     match f {
         Flush::Chunk(v) => w.write_all(&v),
         Flush::Flush => w.flush()
@@ -52,13 +54,46 @@ fn write_flush<W: Write>(w: &mut W, f: Flush<Vec<u8>>) -> Result<()> {
 
 /// A conduit that consumes bytes and writes them to the given `Write`,
 /// while also providing flushing capabilities.
-pub fn writer_flush<'a, W: 'a + Write>(mut w: W) -> ConduitM<'a, Flush<Vec<u8>>, (), Result<()>> {
+pub fn writer_flush<'a, W: 'a + Write>(mut w: W) -> ConduitM<'a, Flush<Vec<u8>>, (), io::Result<()>> {
     consume().and_then(|fo| {
         match fo {
             None => Ok(()).into(),
             Some(f) => match write_flush(&mut w, f) {
                 Err(e) => Err(e).into(),
                 Ok(_) => writer_flush(w)
+            }
+        }
+    })
+}
+
+pub fn receiver<'a, T: 'a>(r: Receiver<T>) -> ConduitM<'a, (), T, ()> {
+    defer().and_then(|_| {
+        match r.recv() {
+            Err(RecvError) => ().into(),
+            Ok(x) => produce(x).and(receiver(r))
+        }
+    })
+}
+
+pub fn sender<'a, T: 'a>(s: Sender<T>) -> ConduitM<'a, T, (), ()> {
+    consume().and_then(|xo| {
+        match xo {
+            None => ().into(),
+            Some(x) => match s.send(x) {
+                Err(SendError(x)) => leftover(x),
+                Ok(_) => sender(s)
+            }
+        }
+    })
+}
+
+pub fn sync_sender<'a, T: 'a>(s: SyncSender<T>) -> ConduitM<'a, T, (), ()> {
+    consume().and_then(|xo| {
+        match xo {
+            None => ().into(),
+            Some(x) => match s.send(x) {
+                Err(SendError(x)) => leftover(x),
+                Ok(_) => sync_sender(s)
             }
         }
     })
